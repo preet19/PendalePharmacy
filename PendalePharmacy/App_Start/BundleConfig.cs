933edc0eca41739bcde99c01941a810dc0572060
Dilpreet Singh <preet19@outlook.com>
2017-06-23 03:49:58 -0400

    change to layout

--- PendalePharmacy/App_Start/BundleConfig.cs
+++ PendalePharmacy/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace PendalePharmacy
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/united.css",
+                      "~/Content/united.min.css",
                        "~/Content/font-awesome.min.css"));
         }
     }
